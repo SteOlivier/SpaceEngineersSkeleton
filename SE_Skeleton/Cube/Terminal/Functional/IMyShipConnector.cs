e530550642d2966c52af37d232650f2a6ea31c82
stefanWtss <stefan@wethinksoftwaresolutions.com>
2020-05-24 19:30:16 +0200

    Added to other item types to the skeleton... Started removing namespace depth...

---
+++ SE_Skeleton/Cube/Terminal/Functional/IMyShipConnector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Skeleton.Cube.Terminal.Functional
+{
+    public interface IMyShipConnector : IMyFunctionalBlock
+    {
+        bool ThrowOut { get; }
+        bool CollectAll { get; }
+        bool IsLocked { get; }
+
+        /**
+            Actions:
+            OnOff -> Toggle block On/Off
+            OnOff_On -> Toggle block On
+            OnOff_Off -> Toggle block Off
+            ThrowOut -> Throw Out On/Off
+            CollectAll -> Collect All On/Off
+            SwitchLock -> Switch lock
+    */
+    }
+}
---
+++ SE_Skeleton/Cube/Terminal/Functional/IMyShipDrill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Skeleton.Cube.Terminal.Functional
+{
+    public interface IMyShipDrill : IMyFunctionalBlock
+    {
+        bool UseConveyorSystem { get; }
+        /**
+            Actions:
+            OnOff -> Toggle block On/Off
+            OnOff_On -> Toggle block On
+            OnOff_Off -> Toggle block Off
+            UseConveyor -> Use Conveyor System On/Off
+        */
+    }
+}
